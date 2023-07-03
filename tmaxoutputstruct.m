92bca437096728c65e2bbcb5006a550c919664a8
Your Name <deckerwill7@gmail.com>
2023-07-02 20:16:31 -0500

    finally got it to work

--- tmaxoutputstruct.m
+++ tmaxoutputstruct.m
@@ -8,32 +8,59 @@ function [tmaxoutput] = tmaxoutputstruct(varargin)
 %   'dir' -- ('yes|no') default: 'no' if you are reading in the
 
 %% begin
-% create empty struct
-tmaxoutput = struct([]);
-
 % get length of varargin for later
 lengvars = length(varargin);
 
 % check if user input 'dir' as a parameter and return its position
-isdir = false;
+isdir = false; % default
 for i=1:lengvars
-    if i == 'dir'
+    if varargin{i} == 'dir'
         isdir = true;
-        dir2use = varargin(i);
     end
 end
+isdir = {isdir};
 
 % if running function using a dir, 
 if isdir == true
-    txtdir = varargin{1};
-    for i = 1:dir2use
-        T = readtable(dir2use{i})
-        T([130:end], :) = [];
-        tmaxoutput(i) = T
+    txtdir = dir([varargin{1} filesep '*.txt']);
+    tmaxstruct = struct([]);
+     for i = 1:numel(txtdir)
+        curr = readtable([txtdir(i).folder filesep txtdir(i).name], 'Delimiter', ' ');
+        curr(130:end, :) = [];
+        datapts = curr{2:end, 2:152};
+        sigpts = datapts <= 0.05;
+        currname = split(txtdir(i).name, '.').';
+        currname = currname(1);
+        tmaxstruct(i).(string(currname)) = curr;
+    
+        sumsigs = 0;
+        for j = 1:length(sigpts)
+            if sigpts(j) == 1
+                sumsigs = sumsigs + 1;
+            end
+        end
+        sumsigs = {sumsigs};
+        tmaxstruct(i).sumsigs = sumsigs;
+     end
+
+elseif dir ~= true
+    curr = readtable(varargin{1}, 'Delimiter', ' ');
+    curr(130:end,:) = [];
+    datapts = curr{2:end, 2:152};
+    sigpts = datapts <= 0.05;
+
+   for i = 1:length(sigpts)
+       if sigpts(i) == 1
+           sumsigs = sumsigs + 1;
+       end
     end
+    sumsigs = {sumsigs};
+    disp(sumsigs(1))
+%    
 end
 
-
+% end of function
+end
 
 
 
