b61b23fdc673feac5f482c95ee1d2e7c61f0ce3a
Your Name <deckerwill7@gmail.com>
2023-09-14 13:57:07 -0500

    working

--- analyzeSME2.m
+++ analyzeSME2.m
@@ -1,4 +1,4 @@
-function [ERP, erpcom, SME] = analyzeSME2(ALLERP, erpnames, strnames, smenames, timerange, txtdir, erpdir)
+function [ERP, erpcom, SME] = analyzeSME2(ALLERP, erpnames, strnames, txtdir, erpdir)
 % ANALYZESME2 Calculate and analyze SME values created during grandaveraging
 % 
 % Parameters
@@ -13,6 +13,8 @@ function [ERP, erpcom, SME] = analyzeSME2(ALLERP, erpnames, strnames, smenames, 
 %
 % timerange: cell, int
 %   custom time range to look into (e.g., timerange = [200 400]) in ms
+%
+% NOTE: indices are 7,8,9 for time range of P2
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % set variables
@@ -20,74 +22,48 @@ erpcom = '';
 ERP    = preloadERP;
 numerpnames = length(erpnames);
 SME = struct();
-%DQ_spec_structure = {'aSME Pool ERPSETs', 'RMS GrandAvg combine Pool ERPSETs', 'RMS GrandAvg combine'}
+% DQ_spec_structure = {'aSME Pool ERPSETs', 'RMS GrandAvg combine Pool ERPSETs', 'RMS GrandAvg combine'};
 target = timerange;
+
 
 % create grandaverage
 for i=1:numerpnames
-    ERP = pop_gaverager([txtdir filesep erpnames{i}], 'DQ_flag', 1, 'DQ_spec', DQ_spec_structure );
+    ERP = pop_gaverager([txtdir filesep erpnames{i}], 'DQ_flag', 1);
     ERP = pop_savemyerp(ERP,...
      'erpname', [strnames{i} 'grandaverage'], 'filename', [strnames{i} 'grandaverage.erp'] , 'filepath', erpdir,...
-     'Warning', 'on');
+     'Warning', 'off');
 end
 
 % get winner
-epoch_start = ERP.times(1); epoch_end = ERP.times(end);
-default_spec = make_DQ_spec([epoch_start epoch_end]);
-custom_aSME_spec = default_spec;
-DQ_n = struct2cell(custom_aSME_spec); %change to cell array
-DQ_n = squeeze(DQ_n(1,:,:));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 for i=1:numerpnames
+    
+    % load in ERP set
+    ERP = pop_loaderp( 'filename', [strnames{i} 'grandaverage.erp'], 'filepath', erpdir );
 
+    % set bins
+    b = {ERP.dataquality(3).data};
+    b1 = b{1, 1}(:, :, 1);
+    b2 = b{1, 1}(:, :, 2);
+    max1 = mean(max(b1(:, 7:9)));
+    min1 = mean(min(b1(1:110, 7:9)));
+    max2 = mean(max(b2(:, 7:9)));
+    min2 = mean(min(b2(1:110, 7:9)));
+    max_hat = (max1 + max2)/ sqrt(2); %divide my sqrt of number of bins...it is hard coded here for this project
+    min_hat = (min1 + min2)/sqrt(2);
+    win = abs(max_hat - min_hat);
 
+    % input into struct
+    SME(i).type = strnames{i};
+    SME(i).val = win;
 
+end
 
+% add to workspace
+assignin('base', 'SME', SME)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+[t,l] = min([SME.val]);
+winner = SME(l).type;
+sprintf('The winner of this operation is %s', winner)
 
 
 end
